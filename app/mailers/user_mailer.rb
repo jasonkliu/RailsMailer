29283d0206aa7d56ce58e03dc659266f2349b419
Jason Liu <jason@jasonkliu.com>
2014-06-16 12:35:16 -0700

    Add user mailer form and format

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/Login'
+    mail(to: @user.email, subject: "Welcome to My Site!")A
+  end
 end
