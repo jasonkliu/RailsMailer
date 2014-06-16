664e872666047bea3b1ff4a4fa52cdfa1065ac7c
Jason Liu <jason@jasonkliu.com>
2014-06-16 12:36:25 -0700

    New scaffold user name email login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529234421) do
+ActiveRecord::Schema.define(version: 20140616193528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,5 +32,13 @@ ActiveRecord::Schema.define(version: 20140529234421) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "login"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
